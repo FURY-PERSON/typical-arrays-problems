0c59c235d8a3f6f7f9b4c08ea55ff3debb408816
Михаил <78750882+FURY-PERSON@users.noreply.github.com>
2021-03-12 21:33:20 +0300

    init: start typical-arrays-problems
    init: start typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+    let minar=0;
+    if(!Array.isArray(array)||array.length==0)
+        return 0;
+    for(let i=0;i<array.length;i++)
+        if (array[i]<minar)
+            minar=array[i];
+  return minar;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let maxar=0;
+    if(!Array.isArray(array)||array.length==0)
+        return 0;
+    for(let i=0;i<array.length;i++)
+        if (array[i]>maxar)
+            maxar=array[i];
+  return maxar;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let avgar=0;
+    if(!Array.isArray(array)||array.length==0)
+        return 0;
+    for(let i=0;i<array.length;i++)
+        avgar+=array[i];
+  return avgar/array.length;
 }
